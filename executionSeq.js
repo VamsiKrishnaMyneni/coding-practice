c86632ff1d58a3f4a9b559013e331cf4c9f7c6d8
Vamsi Krishna Myneni <32959424+VamsiKrishnaMyneni@users.noreply.github.com>
2025-01-16 12:11:31 +0530

    excution sequence of promise, setTimeout

--- executionSeq.js
+++ executionSeq.js
@@ -11,3 +11,18 @@ Promise.resolve().then(() => console.log('E'));
 console.log('F');
 
 setTimeout(() => console.log('G'));
+
+// ==== output ======
+// A
+// F
+// C
+// E
+// B
+// G
+// D
+
+
+// read on event loop
+// read on call stack
+// read on callback queue
+// read on micro, macro task queue in call back que
