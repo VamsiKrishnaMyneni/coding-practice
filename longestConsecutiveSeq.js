04f8cdc1e5d336ea6701dc26c3218421f0a66164
Vamsi Krishna Myneni <32959424+VamsiKrishnaMyneni@users.noreply.github.com>
2025-02-02 23:00:11 +0530

    flatten and accumulate array values with reduce

--- longestConsecutiveSeq.js
+++ longestConsecutiveSeq.js
@@ -18,5 +18,21 @@ const longestConsecutiveSeq = (arr) => {
 
 
 // Example usage:
-const arr = [100, 4, 200, 1, 3, 2];
-console.log(longestConsecutiveSeq(arr)); // Output: 4 for (1, 2, 3, 4)
+const arrValue = [100, 4, 200, 1, 3, 2];
+console.log(longestConsecutiveSeq(arrValue)); // Output: 4 for (1, 2, 3, 4)
+
+// using brute force approach
+
+const longestConsecutiveSeqBruteForce = (arr) => {
+    let maxCount = 0;
+    for (let i = 0; i < arr.length; i++) {
+        let currentCount = 1;
+        let currentNum = arr[i];
+        while (arr.includes(currentNum + 1)) {
+            currentNum++;
+            currentCount++;
+        }
+        maxCount = Math.max(maxCount, currentCount);
+    }
+    return maxCount;
+}
