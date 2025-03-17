46fe7062798257d99bb0efd413ef5cac35cd9e5c
Vamsi Krishna Myneni <32959424+VamsiKrishnaMyneni@users.noreply.github.com>
2025-03-17 15:04:17 +0530

    flatten and accumulate array values with reduce

--- sumArray.js
+++ sumArray.js
@@ -12,3 +12,13 @@ const sumArray = (arr) => {
 
 console.log(sumArray([1, [2, [3, 4], 5]])); // output 15
 
+
+
+const sumArray2 = (arr) => {
+    return arr.reduce((acc, val) => {
+        return acc + (Array.isArray(val) ? sumArray2(val) : val);
+    }, 0);
+}
+
+
+console.log(sumArray2([1, [2, [3, 4], 5]])); // output 15
