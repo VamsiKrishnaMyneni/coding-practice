8ea96690329572a5f0b28622f624b913ef37c0e9
Vamsi Krishna Myneni <32959424+VamsiKrishnaMyneni@users.noreply.github.com>
2025-03-19 15:45:08 +0530

    findNthHighestElement

---
+++ findNthHighestElement.js
@@ -0,0 +1,15 @@
+(function () {
+
+    const findNthHighestElement = (arr, ind) => {
+        let n = arr.length;
+        for (let i = 0; i < n - 1; i++) {
+            for (let j = 0; j < n - 1 - i; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+                }
+            }
+        }
+        return arr[ind];
+    }
+    console.log(findNthHighestElement([1, 4, 5, 2, 6], 3))
+})();
