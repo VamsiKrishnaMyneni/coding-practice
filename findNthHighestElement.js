aa0797789d5cb08f6ef1a3011c5853119e10dfd2
Vamsi Krishna Myneni <32959424+VamsiKrishnaMyneni@users.noreply.github.com>
2025-03-19 15:47:19 +0530

    findNthHighestElement

--- findNthHighestElement.js
+++ findNthHighestElement.js
@@ -1,10 +1,11 @@
 (function () {
 
     const findNthHighestElement = (arr, ind) => {
+        // used bubble sort reverse
         let n = arr.length;
         for (let i = 0; i < n - 1; i++) {
             for (let j = 0; j < n - 1 - i; j++) {
-                if (arr[j] < arr[j + 1]) {
+                if (arr[j] < arr[j + 1]) { // reversing sort
                     [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
                 }
             }
