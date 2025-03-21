bb7c270f904742efe196dd7dde885b29425183df
Vamsi Krishna Myneni <32959424+VamsiKrishnaMyneni@users.noreply.github.com>
2025-03-21 13:04:29 +0530

    remoove duplicates

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -2,23 +2,28 @@ const arr = [1, 2, 4, 7, 1, 5, 5, 2];
 // remove the duplicate elements from the array
 
 // solution-1
-const result = new Set(arr);
 
-console.log([...result]); // Set { 1, 2, 4, 7, 5 }
+(() => {
+    const result = new Set(arr);
+    console.log([...result]); // Set { 1, 2, 4, 7, 5 }
+})();
+
 
 // solution-2
+(() => {
+    const uniqueArr = arr.filter((item, index) => arr.indexOf(item) === index);
+    console.log(uniqueArr); // [ 1, 2, 4, 7, 5 ]
+})();
 
-const uniqueArr = arr.filter((item, index) => arr.indexOf(item) === index);
-console.log(uniqueArr); // [ 1, 2, 4, 7, 5 ]
 
 // solution-3
+(() => {
+    const finalArr = [];
 
-const finalArr = [];
-
-for (let i = 0; i < arr.length; i++) {
-    if (!finalArr.includes(arr[i])) {
-        finalArr.push(arr[i]);
+    for (let i = 0; i < arr.length; i++) {
+        if (!finalArr.includes(arr[i])) {
+            finalArr.push(arr[i]);
+        }
     }
-}
-
-console.log(finalArr); // [ 1, 2, 4, 7, 5 ]
+    console.log(finalArr); // [ 1, 2, 4, 7, 5 ]
+})();
