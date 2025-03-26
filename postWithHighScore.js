deb8d258aa70632b71551c1a3a2123803c19a4ee
Vamsi Krishna Myneni <32959424+VamsiKrishnaMyneni@users.noreply.github.com>
2025-03-26 19:08:34 +0530

    post with highest score

--- postWithHighScore.js
+++ postWithHighScore.js
@@ -1,17 +1,14 @@
 const posts = [
-    { id: 1, title: 'Post 1', likes: 15, Comment: 3, share: 10 },
-    { id: 2, title: 'Post 2', likes: 20, Comment: 2, share: 5 },
-    { id: 3, title: 'Post 3', likes: 25, Comment: 5, share: 15 },
-    { id: 4, title: 'Post 4', likes: 30, Comment: 4, share: 20 }];
+    { id: 1, title: 'Post 1', likes: 0, Comment: 0, share: 0 }];
 
 // Find the post with the highest score
 // for like 2points, comment 3points, share 5points
 // The post with the highest score is Post 4
 // The score is 30(likes) + 4(Comment) + 20(share) = 54
 
-const maxScorePost = (posts) => posts.reduce((max, post) => {
+const maxScorePost = (posts) => posts.reduce((max, post, index) => {
     const score = post.likes * 2 + post.Comment * 3 + post.share * 5;
-    return score > max.score ? { id: post.id, title: post.title, score } : max;
+    return (score > max.score || index === 0) ? { id: post.id, title: post.title, score } : max;
 }, { id: 0, title: '', score: 0 });
 
 const postWithHighScore = (posts) => {
@@ -28,6 +25,4 @@ const postWithHighScore = (posts) => {
 console.log(
     maxScorePost(posts), // { id: 4, title: 'Post 4', score: 54 }
     postWithHighScore(posts) // { id: 4, title: 'Post 4', score: 54 }
-);
-maxScorePost(posts); // { id: 4, title: 'Post 4', score: 54 }
-postWithHighScore(posts); // { id: 4, title: 'Post 4', score: 54 }
+); 
