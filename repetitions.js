b62aa1b751331c24806200095b60b18bb369ca3d
Vamsi Krishna Myneni <32959424+VamsiKrishnaMyneni@users.noreply.github.com>
2025-03-27 16:24:07 +0530

    char repeate count

--- repetitions.js
+++ repetitions.js
@@ -26,3 +26,13 @@ const getRepetitionsCount = (str) => {
 }
 
 console.log(getRepetitionsCount(str));  // { j: 1, a: 2, v: 1, s: 1, c: 1, r: 1, i: 1, p: 1, t: 1 }
+
+const getRepetitionsCountReduce = (str) => {
+    return str.split('').reduce((acc, letter) => {
+        acc[letter] = (acc[letter] || 0) + 1;
+        return acc;
+    }, {});
+
+}
+
+console.log(getRepetitionsCountReduce(str));  // { j: 1, a: 2, v: 1, s: 1, c: 1, r: 1, i: 1, p: 1, t: 1 }
