20c90b03e25ef07c8e4b565c309f6f1473dffdd8
Vamsi Krishna Myneni <32959424+VamsiKrishnaMyneni@users.noreply.github.com>
2025-03-28 17:47:54 +0530

    altStringCharConcat

--- altStringCharConcat.js
+++ altStringCharConcat.js
@@ -6,6 +6,29 @@ let word1 = 'abc',
     word2 = 'pqrr';
 
 function addStralternatively(str1, str2) {
+    let largeStr = str1;
+    let remain = '';
+    if (str1 > str2) {
+        largeStr = str2;
+        remain = str1.slice(str2.length);
+    }
+    if (str2 > str1) {
+        largeStr = str1;
+        remain = str2.slice(str1.length);
+    }
+    const resultStr = [];
+    for (let i = 0; i < largeStr.length; i++) {
+        resultStr.push(str1[i], str2[i]);
+    }
+    resultStr.push(remain);
+    return resultStr.join('');
+}
+console.log(addStralternatively(word1, word2));
+
+// Output: "apbqcr"
+
+
+function addStralternatively2(str1, str2) {
     let resultStr = "";
     let maxLength = Math.max(str1.length, str2.length);
     for (let i = 0; i < maxLength; i++) {
@@ -14,6 +37,4 @@ function addStralternatively(str1, str2) {
     }
     return resultStr;
 }
-console.log(addStralternatively(word1, word2));
-
-// Output: "apbqcr"
+console.log(addStralternatively2(word1, word2));
