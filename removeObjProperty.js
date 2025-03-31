1fa3e5261ff0536afdfb1c723d68ad9a2615bb03
Vamsi Krishna Myneni <32959424+VamsiKrishnaMyneni@users.noreply.github.com>
2025-03-31 10:40:24 +0530

    removeObjProperty

--- removeObjProperty.js
+++ removeObjProperty.js
@@ -2,7 +2,8 @@ const removeObjProperty = (obj, prop) => {
     if (obj.hasOwnProperty(prop)) {
         const { [prop]: _, ...rest } = obj;
         console.log({ [prop]: _ }); // This will log the new object with the specified property
-
+        // console.log(rest); // This will log the original object with the specified property
+        // delete obj[prop]; // This line would modify the original object
         return rest;
     }
     return obj;
