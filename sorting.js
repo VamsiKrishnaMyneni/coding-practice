d453a371fd23aba74d9ff727d7d2ac369e8b4861
Vamsi Krishna Myneni <32959424+VamsiKrishnaMyneni@users.noreply.github.com>
2025-04-03 07:51:58 +0530

    JS sorting for string

--- sorting.js
+++ sorting.js
@@ -1,9 +1,16 @@
 const unsortedArray = [5, 2, 9, 1, 5, 6];
+const stringArray = ["Banana", "Orange", "Apple", "Mango"];
+
 
 console.log(unsortedArray.sort()); // ascending order
 
 console.log(unsortedArray.sort((a, b) => a - b)) // ascending order
 
 console.log(unsortedArray.sort((a, b) => b - a)) // descending order
+
+
+
+console.log(stringArray.sort()); // ascending order 
+
 
 
