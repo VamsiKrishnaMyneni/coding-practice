c56d364b28af3192e0a68d97ad4a72b90573ae57
Vamsi Krishna Myneni <32959424+VamsiKrishnaMyneni@users.noreply.github.com>
2025-04-05 00:32:00 +0530

    reverse the string

--- reverseString.js
+++ reverseString.js
@@ -1,23 +1,14 @@
-/// given a string reverse the words in the string individually
+(function () {
+    const jsWorldStr = "Welcome to Javascript world!";
 
-// emocleW ot tpircsavaJ !dlrow
+    function reverseStr(parm_str) {
+        let result = "";
 
-
-const welcomeStr = "Welcome to Javascript world!";
-
-
-function reverseArrayStr(parm_str) {
-    const strArr = parm_str.split(" ");
-    let result = "";
-
-    for (let i = 0; i < strArr.length; i++) {
-        for (let j = strArr[i].length - 1; j >= 0; j--) {
-            result += strArr[i][j];
+        for (let i = parm_str.length - 1; i >= 0; i--) {
+            result += parm_str[i];
         }
-        result += " ";
+        return result;
     }
 
-    return result;
-}
-
-console.log(reverseArrayStr(welcomeStr));  //emocleW ot tpircsavaJ !dlrow 
+    console.log(reverseStr(jsWorldStr));  //!dlrow tpircsavaJ ot emocleW
+})()
