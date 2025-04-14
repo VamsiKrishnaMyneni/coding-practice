2a4f47cc60d7a9a1a1fb1390e568778cfc8d3c27
Vamsi Krishna Myneni <32959424+VamsiKrishnaMyneni@users.noreply.github.com>
2025-04-14 10:32:42 +0530

    custom filter function

---
+++ 30dayJSCoding/customFilter.js
@@ -0,0 +1,34 @@
+/* 
+Given an integer array arr and a filtering function fn, return a filtered array filteredArr.
+
+The fn function takes one or two arguments:
+
+arr[i] - number from the arr
+i - index of arr[i]
+filteredArr should only contain the elements from the arr for which the expression fn(arr[i], i) evaluates to a truthy value. A truthy value is a value where Boolean(value) returns true.
+
+Please solve it without the built-in Array.filter method.
+*/
+
+
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+(function () {
+    const filter = function (arr, fn) {
+        const result = [];
+        if (!Array.isArray(arr)) {
+            throw new Error('Not an array.');
+        }
+        for (let i = 0; i < arr.length; i++) {
+            result.push(fn(arr[i], i))
+        }
+
+        return result;
+    };
+
+    console.log(filter([1, 2, 3], function (n, i) { return i === 0; }));
+
+})();
