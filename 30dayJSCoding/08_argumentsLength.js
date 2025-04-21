a9ae800c93bc9a11c1dce00b4a67e4fc31b1d5b9
Vamsi Krishna Myneni <32959424+VamsiKrishnaMyneni@users.noreply.github.com>
2025-04-21 17:13:47 +0530

    argumentsLength

--- 30dayJSCoding/08_argumentsLength.js
+++ 30dayJSCoding/08_argumentsLength.js
@@ -1,7 +1,33 @@
+/*
+There are two primary approaches to this problem:
+
+1. using rest parameters
+2. using the arguments object
+URL: https://leetcode.com/problems/return-length-of-arguments-passed/solutions/5722508/more-than-you-ever-wanted-to-know-about-this-topic/?envType=study-plan-v2&envId=30-days-of-javascript
+*/
+
+// Solution-1
 (function () {
 
     var argumentsLength = function (...args) {
         return args?.length || 0;
     };
+    console.log(argumentsLength(1, 2, 3)); // 3
+})();
+
+// Solution-2
+(function () {
+
+    var argumentsLength = function () {
+        return arguments?.length || 0;
+    };
+    console.log(argumentsLength(1, 2, 3)); // 3
+})();
+
+// Solution-3
+(function () {
+    // what happens if we use arguments object in a arrow function
+    var argumentsLength = () => arguments?.length || 0;
+
     console.log(argumentsLength(1, 2, 3)); // 3
 })();
