7394cc87535cc0b99f045a3a96baf153a8612a35
Vamsi Krishna Myneni <32959424+VamsiKrishnaMyneni@users.noreply.github.com>
2025-04-30 10:06:19 +0530

    memoize function with object

--- 30dayJSCoding/10_memoise.js
+++ 30dayJSCoding/10_memoise.js
@@ -18,6 +18,13 @@ Given a function fn, return a memoized version of that function.
             return result;
         }
     }
+
+    // Example usage:
+    let callCount = 0;
+    const memoizedFn = memoize((a, b) => a + b)
+    memoizedFn(2, 3) // 5
+    memoizedFn(2, 3) // 5
+
 })
 
     // solution 2;
